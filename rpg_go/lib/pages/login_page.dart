86525b63c1bf8041885afda4356147c31d623d64
Arthur Pfeilsticker <arthurlfpfeil@gmail.com>
2024-06-16 19:40:33 -0300

    fix: bugs

--- rpg_go/lib/pages/login_page.dart
+++ rpg_go/lib/pages/login_page.dart
@@ -152,8 +152,6 @@ class _LoginPageState extends State<LoginPage> {
     );
 
     if (response.statusCode == 200) {
-      // If the server did return a 200 CREATED response,
-      // then parse the JSON.
       // print(response.body);
       globals.loggedUser = User.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
       Database db = await SQLiteModel.instance.database;
@@ -168,8 +166,6 @@ class _LoginPageState extends State<LoginPage> {
       // print('$savedName is logged with id: $savedId and password: $password on local DataBase!');
       return true;
     } else {
-      // If the server did not return a 201 CREATED response,
-      // then throw an exception.
       throw Exception('Requisition Failed - Login.');
     }
   }
